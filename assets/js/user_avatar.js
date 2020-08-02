d54622632e096a40496d0cd9be558f3af8e8a5d6
wanxkdnvwsk <wawlnmzda@163.com>
2020-08-02 20:29:23 +0800

    完成user整体功能

--- assets/js/user_avatar.js
+++ assets/js/user_avatar.js
@@ -1,19 +1,62 @@
-$(function(){
+$(function () {
     // 1.1 获取裁剪区域的 DOM 元素
-  var $image = $('#image')
-  // 1.2 配置选项
-  const options = {
-    // 纵横比
-    aspectRatio: 1,
-    // 指定预览区域
-    preview: '.img-preview'
-  }
+    var $image = $('#image')
+    // 1.2 配置选项
+    const options = {
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域
+        preview: '.img-preview'
+    }
 
-  // 1.3 创建裁剪区域
-  $image.cropper(options)
+    // 1.3 创建裁剪区域
+    $image.cropper(options)
 
 
-$('#btnsc').on('click',function(){
-    
-})
+    $('#btnsc').on('click', function () {
+        $('#file').click()
+    })
+
+    $('#file').on('change', function () {
+        let files = $('#file')[0].files
+
+        if (files.length <= 0) {
+            layui.layer.msg('请选择图片')
+        }
+        let file = $('#file')[0].files[0]
+
+        var newImgURL = URL.createObjectURL(file)
+
+
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', newImgURL)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
+
+
+    $('#btnUpload').on('click', function () {
+
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+
+
+        $.ajax({
+            type: 'post',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status == 0) {
+                    layui.layer.msg(res.message)
+                    window.parent.getUserInfo()
+                }
+            }
+        })
+    })
 })
