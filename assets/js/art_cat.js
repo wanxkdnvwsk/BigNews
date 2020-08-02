21ae1e70d9a66f292b03a28ada41910b7ba36941
wanxkdnvwsk <wawlnmzda@163.com>
2020-08-02 22:26:03 +0800

    编辑表单初始化不会

--- assets/js/art_cat.js
+++ assets/js/art_cat.js
@@ -1,4 +1,7 @@
 $(function(){
+    let layer =  layui.layer
+    let form = layui.form
+    let indexadd=null
     // 渲染分类列表
     getCat()
     function getCat(){
@@ -15,13 +18,56 @@ $(function(){
         })
     }
 
-
+  //   添加弹出层
     $('#add-btn').on('click',function(){
-        layer.open({
+        indexadd= layer.open({
             type:1,
             area: ['500px', '300px'],
-            title: '添加文章类别',
-            content: '可以填写任意的layer代码',
+            title: '添加文章分类',
+            content: $('#tc-add').html(),
           });     
     })
+
+    
+    // 提交添加
+    $('body').on('submit','#add-form',function(e){
+        e.preventDefault()
+        let data = $(this).serialize()
+        // console.log(data)
+
+        $.ajax({
+            type:'post',
+            url:'/my/article/addcates',
+            data:data,
+            success:function(res){
+               if(res.status==0){
+                   layer.msg(res.message)
+                   layer.close(indexadd)
+                   getCat()
+                   
+               }
+            }
+        })
+    })
+
+
+    $('tbody').on('click','#btn-edit',function(){
+       layer.open({
+            type:1,
+            area: ['500px', '300px'],
+            title: '修改文章分类',
+            content: $('#tc-edit').html(),
+        }); 
+
+        $.ajax({
+            type:'get',
+            url:'/my/article/cates',
+            success:function(res){
+                if(res.status==0){
+                    form.val("editForm",res.data)
+                }
+            }
+        })
+    })
+
 })
